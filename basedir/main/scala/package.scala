f8d6a583a25d452267db2254cace3c4d31dd8fe3
Christian Krause <kizkizzbangbang@googlemail.com>
2013-08-12 10:33:18 +0200

    package basedir scaladoc

---
+++ basedir/main/scala/package.scala
@@ -0,0 +1,25 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                               *
+ *  This file is part of the project 'scala-xdg'.                                                *
+ *                                                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                               *
+ *  This project is free software. It comes without any warranty, to the extent permitted by     *
+ *  applicable law. You can redistribute it and/or modify it under the terms of the Do What The  *
+ *  Fuck You Want To Public License, Version 2, as published by Sam Hocevar.                     *
+ *                                                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                               *
+ *  See http://sam.zoy.org/wtfpl/COPYING for more details.                                       *
+ *                                                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+package org.freedesktop
+
+/** This package contains an implementation of the
+  * [[http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html XDG Base Directory
+  * Specification]].
+  */
+package object basedir {
+}
